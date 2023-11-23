ceb7951ce52d65e838b07b597d4e8cb7ebbe4c13
eoqna <eoqna@naver.com>
2023-11-23 15:47:23 +0900

    Redux 기본 세팅

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import "antd/dist/antd.css";
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers/index";
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <App />
+root.render (
+  <Provider store={createStoreWithMiddleware(Reducer)}>
+    <App />
+  </Provider>
 );
